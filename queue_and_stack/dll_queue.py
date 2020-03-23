74ccba68b981a2cfcccaa705df52e12e61057e76
PhatDeluxe <ethan@skamarock.net>
2020-03-23 13:57:41 -0600

    Monday's homework

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -2,18 +2,31 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
-
+''' 
+Were going to use a dll to make the queue
+This allows us to have a head and a tail
+We can also create arrays of dynamic size
+without using insane ammounts of memory
+'''
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # It is good for making arrays of dynamic length
+        # also we already wrote the code for DLL so we
+        # save a lot of time not rewriting code
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        ''' Adds a value to the end of the queue '''
+        self.storage.add_to_tail(value)
+        
 
     def dequeue(self):
-        pass
+        ''' Removes and returns the first value in the queue '''
+        value = self.storage.remove_from_head()
+        return value
 
     def len(self):
-        pass
+        ''' returns the number of values in the queue '''
+        return self.storage.length
